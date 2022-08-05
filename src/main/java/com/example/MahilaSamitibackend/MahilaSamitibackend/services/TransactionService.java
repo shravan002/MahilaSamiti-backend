dbc23182ff2d6b2c36e1df746d3e5cf16f598be5
shravan <kshravanoo2@gmail.com>
2022-08-05 10:12:27 +0530

    transaction handling

--- src/main/java/com/example/MahilaSamitibackend/MahilaSamitibackend/services/TransactionService.java
+++ src/main/java/com/example/MahilaSamitibackend/MahilaSamitibackend/services/TransactionService.java
@@ -1,5 +1,7 @@
 package com.example.MahilaSamitibackend.MahilaSamitibackend.services;
 
+import com.example.MahilaSamitibackend.MahilaSamitibackend.entities.Committee;
+import com.example.MahilaSamitibackend.MahilaSamitibackend.entities.Member;
 import com.example.MahilaSamitibackend.MahilaSamitibackend.entities.Transaction;
 
 import java.util.List;
@@ -8,4 +10,14 @@ public interface TransactionService {
     Transaction addTransaction(Transaction transaction);
 
     List<Transaction> getTransactions(String memberId);
+
+
+    //todo return type?
+    Member sanctionLoan(long amount,Member member, Committee committee);
+
+    Member minimumDuePayment(long amount, Member member, Committee committee);
+
+    Member outstandingAmountPayment(long amount, Member member, Committee committee);
+
+    Member cyclicPayment(long amount, Member member, Committee committee);
 }
